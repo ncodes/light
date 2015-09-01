e3b7a1a5195fac58e2730b31eb12329384da78f3
Kennedy Idialu <kennedyidialu@gmail.com>
2015-09-01 14:12:54 +0100

    read-only access to session from view

--- app/config/view_helpers.js
+++ app/config/view_helpers.js
@@ -6,5 +6,12 @@ module.exports = {
 	
 	log: function(m){
 		light.log.debug(m)
+	},
+
+	// session helper (only read-only access)
+	session: {
+		get: function(key) {
+			return light._req.session[key];
+		}
 	}
 }
