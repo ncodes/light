ce2c73018064b72b9a8019139c20988afd4fbe23
Kennedy Idialu <kennedyidialu@gmail.com>
2015-12-13 18:57:43 +0100

    removed next parameter

--- api/helper.js
+++ api/helper.js
@@ -7,10 +7,10 @@
  	// render a static view file
  	static: function(viewName, data) {
  		var data = data || {}
-	 	return function(req, res, next) {
+	 	return function(req, res) {
 			var flashData = req.flash() || {};
 	 		_.extend(data, flashData)
-	 		res.show(viewName, flashData)
+	 		return res.show(viewName, flashData)
 	 	}
 	 }
 }
