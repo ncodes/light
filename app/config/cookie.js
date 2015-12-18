560be2bb57738736896851e743b8be6c815ff473
Kennedy Idialu <kennedyidialu@gmail.com>
2015-12-18 09:11:49 +0100

    Redactor now uses credis client object from `redid-url` module

--- app/config/cookie.js
+++ app/config/cookie.js
@@ -8,5 +8,8 @@ module.exports = {
  	secret: process.env.COOKIE_SECRET || "secret",
 
  	// cookie max age
- 	maxAge: 3600000
+ 	maxAge: 3600000,
+
+ 	// whether to use secured cookie for https
+ 	securedCookie: (process.env.SECURED_COOKIE) ? true : false
 }
